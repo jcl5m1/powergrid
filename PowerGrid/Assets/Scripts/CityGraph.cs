673c293cade79c51d7e13866abdd0ec64b9a1a15
Johnny Lee <jcl5m1@gmail.com>
2015-10-15 21:56:41 -0700

    adding main project

---
+++ PowerGrid/Assets/Scripts/CityGraph.cs
@@ -0,0 +1,225 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityGraph : MonoBehaviour {
+
+	private City[] cities = null;
+	private ArrayList edges = null;
+
+
+	// Use this for initialization
+	void Start () {
+		InitializeCities ();
+
+	}
+
+	void InitializeCities() {
+		//create board
+		cities = FindObjectsOfType(typeof(City)) as City[];
+		edges = new ArrayList ();
+		
+		AddEdge ("Boston", "NewYork", 3);
+		AddEdge ("Philadelphia", "NewYork", 0);
+		AddEdge ("Buffalo", "NewYork", 8);
+		AddEdge ("Philadelphia", "Washington", 3);
+		AddEdge ("Pittsburgh", "Washington", 6);
+		AddEdge ("Norfolk", "Washington", 5);
+		AddEdge ("Norfolk", "Raleigh", 3);
+		AddEdge ("Pittsburgh", "Raleigh", 7);
+		AddEdge ("Buffalo", "Pittsburgh", 7);
+		AddEdge ("Buffalo", "Detroit", 7);
+		AddEdge ("Pittsburgh", "Detroit", 6);
+		
+		AddEdge ("Savannah", "Raleigh", 7);
+		AddEdge ("Atlanta", "Raleigh", 7);
+		AddEdge ("Atlanta", "Knoxville", 5);
+		AddEdge ("Cincinnati", "Knoxville", 6);
+		AddEdge ("Cincinnati", "Raleigh", 15);
+		AddEdge ("Cincinnati", "Pittsburgh", 7);
+		AddEdge ("Cincinnati", "Detroit", 4);
+		AddEdge ("Cincinnati", "Chicago", 7);
+		AddEdge ("Cincinnati", "StLouis", 12);
+		
+		AddEdge ("Savannah", "Jacksonville", 0);
+		AddEdge ("NewOrleans", "Jacksonville", 16);
+		AddEdge ("Birmingham", "Jacksonville", 9);
+		AddEdge ("Tampa", "Jacksonville", 4);
+		AddEdge ("Tampa", "Miami", 4);
+		
+		AddEdge ("Atlanta", "Birmingham", 3);
+		AddEdge ("Atlanta", "StLouis", 12);
+		AddEdge ("Atlanta", "Savannah", 7);
+		
+		AddEdge ("NewOrleans", "Birmingham", 11);
+		AddEdge ("NewOrleans", "Memphis", 7);
+		AddEdge ("NewOrleans", "Dallas", 12);
+		AddEdge ("NewOrleans", "Houston", 8);
+		
+		AddEdge ("Memphis", "Birmingham", 6);
+		AddEdge ("Memphis", "StLouis", 7);
+		AddEdge ("Memphis", "KansasCity", 12);
+		AddEdge ("Memphis", "OklahomaCity", 14);
+		AddEdge ("Memphis", "Dallas", 12);
+		AddEdge ("Dallas", "Houston", 5);
+		AddEdge ("SantaFe", "Houston", 21);
+		AddEdge ("Dallas", "OklahomaCity", 3);
+		AddEdge ("KansasCity", "OklahomaCity", 8);
+		AddEdge ("KansasCity", "SantaFe", 16);
+		AddEdge ("KansasCity", "StLouis", 6);
+		AddEdge ("KansasCity", "Chicago", 8);
+		AddEdge ("KansasCity", "Omaha", 5);
+		AddEdge ("KansasCity", "Denver", 16);
+		
+		AddEdge ("Chicago", "Detroit", 7);
+		AddEdge ("Chicago", "Duluth", 12);
+		AddEdge ("Chicago", "Minneapolis", 8);
+		AddEdge ("Chicago", "Omaha", 13);
+		
+		AddEdge ("Duluth", "Detroit", 15);
+		AddEdge ("Duluth", "Fargo", 6);
+		AddEdge ("Duluth", "Minneapolis", 5);
+		AddEdge ("Minneapolis", "Fargo", 6);
+		AddEdge ("Billings", "Fargo", 17);
+		AddEdge ("Billings", "Minneapolis", 18);
+		
+		AddEdge ("Cheyenne", "Minneapolis", 18);
+		AddEdge ("Cheyenne", "Omaha", 14);
+		AddEdge ("Cheyenne", "Denver", 0);
+		AddEdge ("Cheyenne", "Billings", 9);
+		AddEdge ("Cheyenne", "Boise", 24);
+		
+		AddEdge ("Boise", "Billings", 12);
+		AddEdge ("Boise", "SaltLakeCity", 8);
+		AddEdge ("Boise", "SanFrancisco", 23);
+		AddEdge ("Boise", "Portland", 13);
+		AddEdge ("Seattle", "Billings", 9);
+		AddEdge ("Seattle", "Boise", 12);
+		AddEdge ("Seattle", "Portland", 3);
+		AddEdge ("Portland", "SanFrancisco", 24);
+		AddEdge ("SaltLakeCity", "SanFrancisco", 27);
+		AddEdge ("LasVegas", "SanFrancisco", 14);
+		AddEdge ("LosAngeles", "SanFrancisco", 9);
+		AddEdge ("LosAngeles", "LasVegas", 9);
+		AddEdge ("LosAngeles", "SanDiego", 3);
+		AddEdge ("LasVegas", "SanDiego", 9);
+		AddEdge ("Pheonix", "SanDiego", 14);
+		AddEdge ("Pheonix", "SantaFe", 18);
+		AddEdge ("Pheonix", "LasVegas", 15);
+		
+		AddEdge ("SantaFe", "LasVegas", 27);
+		AddEdge ("SantaFe", "SaltLakeCity", 28);
+		AddEdge ("SantaFe", "Denver", 13);
+		AddEdge ("SantaFe", "OklahomaCity", 15);
+		AddEdge ("SantaFe", "Dallas", 16);
+		AddEdge ("SantaFe", "Houston", 21);
+	}
+	
+	void AddEdge(string city1, string city2, int cost) {
+		
+		int index1 = -1;
+		int index2 = -1;
+		
+		for(int i = 0; i < cities.Length; i++)
+		{
+			if( cities[i].gameObject.name.CompareTo(city1)==0) {
+				index1 = i;
+			}
+			if( cities[i].gameObject.name.CompareTo(city2)==0) {
+				index2 = i;
+			}
+		}
+		
+		if (index1 == -1) {
+			print ("Could not find a city:" + city1);
+			return;
+		}
+		
+		if (index2 == -1) {
+			print ("Could not find a city:" + city2);
+			return;
+		}
+		
+		Edge e = new Edge (cities[index1], cities[index2], cost);
+		edges.Add (e);
+		if (cities [index1].edges == null)
+			cities [index1].edges = new ArrayList ();
+		if (cities [index2].edges == null)
+			cities [index2].edges = new ArrayList ();
+		cities[index1].edges.Add (e);
+		cities[index2].edges.Add (e);
+	}
+
+	public int ComputeCityCost(City c, int step, Player player) {
+				
+		//if player has no cities, just current step value of city
+		//if city is not available at step, return failure
+		//if player has city, it is the cheapest combined edge + step value of city
+
+		int cityCost = c.Cost (step);
+
+		//occupied or already owned
+		if (cityCost == -1)
+			return -1;
+
+		if (c.PlayerIsOwner (player))
+			return -1;
+
+		//at city cost, if no current cities
+		if (player.cities.Count == 0)
+			return cityCost;
+
+		//must be connected to a player owned city
+		//or a connected occupied city?
+
+		//if player has cities, 
+		//we do graph search and find cheapest combinedEdge 
+		//from target city to player owned cities
+		ArrayList combinedEdges = new ArrayList();
+		ArrayList queuedCity = new ArrayList ();
+		queuedCity.Add (c);
+
+		while(queuedCity.Count > 0){
+			City cty = (City)queuedCity[0];
+			queuedCity.RemoveAt(0);
+			cty.beenVisited = true;
+			foreach (Edge e in cty.edges) {
+				City otherCity;
+				if(e.start == cty)
+					otherCity = e.end;
+				else
+					otherCity = e.start;
+
+				if(otherCity.beenVisited)
+					continue;
+				if(otherCity.Occupancy() == 0)
+					continue;
+				if(otherCity.PlayerIsOwner(player))
+				   e.stopSearch = true;
+				else
+					queuedCity.Add(otherCity);
+
+				combinedEdges.Add (e);
+			}
+		}
+
+		int minCost = int.MaxValue;
+		foreach(Edge e in combinedEdges) {
+			if(e.cost < minCost)
+				minCost = e.cost;
+		}
+
+		print ("city cost function is incomplete");
+		return minCost;
+	}
+
+	public void ResetSearchFlags() {
+		foreach(City c in cities)
+			c.beenVisited = false;
+		foreach (Edge e in edges)
+			e.stopSearch = false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ PowerGrid/Assets/Scripts/GameState.cs
@@ -0,0 +1,328 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public enum State {
+		ComputeTurn,
+		BuyPlants,
+		BuyMaterials,
+		BuildCities,
+		Bureaucracy,
+	};
+	
+	private State currentState = State.ComputeTurn;
+	private ArrayList players = null;
+	private ArrayList drawDeckPowerPlants = null;
+	private ArrayList inMarketPowerPlants = null;
+		
+	private int marketCount = 8;
+	private int biddableCount = 4;
+	private int gameRound = 1;
+
+	private int maxPowerPlants = 3;
+	private int gameEndCityCount = 17;
+	private int step2CityCount = 7;
+	private int randomlyRemovedCards = 4;
+	private int numberOfRegions = 3;
+	public int[] cityCountPayoutTable;
+	private int gameStep = 1;
+
+	private int playerTurn = 0;
+
+	public GameObject powerPlantPrefab;
+	public GameObject cityPrefab;
+
+	private PowerPlantMaterialStore materialStore;
+
+	// Use this for initialization
+	void Start () {
+		print ("Initialize game");
+
+		//setup players
+		players = new ArrayList();
+
+		//create objects from GUI setup
+		Player[] p = FindObjectsOfType(typeof(Player)) as Player[];
+		for (int i = 0; i < p.Length; i++) {
+			players .Add (p [i]);
+		}
+		players.Sort ();
+		players.Reverse ();
+
+		//create material store
+		materialStore = new PowerPlantMaterialStore ();
+
+		InitializePayoutTable ();
+		InitializePowerPlants ();
+		DealCards ();
+
+		ShufflePowerPlantCards ();
+	}
+
+	void Reset() {
+		foreach (Player p in players)
+			p.Reset ();
+
+		inMarketPowerPlants.Clear ();
+		drawDeckPowerPlants.Clear ();
+		InitializePowerPlants ();
+		DealCards ();
+
+		ShufflePowerPlantCards ();
+	}
+
+	void InitializePayoutTable() {
+		cityCountPayoutTable = new int[21];
+		cityCountPayoutTable [0] = 10;
+		cityCountPayoutTable [1] = 22;
+		cityCountPayoutTable [2] = 33;
+		cityCountPayoutTable [3] = 44;
+		cityCountPayoutTable [4] = 54;
+		cityCountPayoutTable [5] = 64;
+		cityCountPayoutTable [6] = 73;
+		cityCountPayoutTable [7] = 82;
+		cityCountPayoutTable [8] = 90;
+		cityCountPayoutTable [9] = 98;
+		cityCountPayoutTable [10] = 105;
+		cityCountPayoutTable [11] = 112;
+		cityCountPayoutTable [12] = 118;
+		cityCountPayoutTable [13] = 124;
+		cityCountPayoutTable [14] = 129;
+		cityCountPayoutTable [15] = 134;
+		cityCountPayoutTable [16] = 138;
+		cityCountPayoutTable [17] = 142;
+		cityCountPayoutTable [18] = 145;
+		cityCountPayoutTable [19] = 148;
+		cityCountPayoutTable [20] = 150;
+	}
+
+
+
+	void MovePowerPlantCard(int fromPosition, int toPosition) {
+
+		if (toPosition == fromPosition)
+			return;
+		drawDeckPowerPlants.Insert (toPosition, drawDeckPowerPlants [fromPosition]);
+
+		if (toPosition < fromPosition) 
+			drawDeckPowerPlants.RemoveAt (fromPosition+1);
+		else
+			drawDeckPowerPlants.RemoveAt (fromPosition);
+	}
+
+	int FindIndexOfPowerPlantCard(int cost) {
+		for (int i = 0; i < drawDeckPowerPlants.Count; i++) {
+			if( ((PowerPlant)drawDeckPowerPlants[i]).baseCost == cost)
+				return i;			
+		}
+		return -1;
+	}
+
+	void ShufflePowerPlantCards() {
+		ArrayList shuffled = new ArrayList();
+
+		int randomIndex = 0;
+		while (drawDeckPowerPlants.Count > 0)
+		{
+			randomIndex = Random.Range(0, drawDeckPowerPlants.Count); //Choose a random object in the list
+			shuffled.Add(drawDeckPowerPlants[randomIndex]); //add it to the new, random list
+			drawDeckPowerPlants.RemoveAt(randomIndex); //remove to avoid duplicates
+		}
+
+		drawDeckPowerPlants = shuffled;
+
+		//put step 3 on bottom
+		int index = FindIndexOfPowerPlantCard (100);
+		if (index != -1)
+			MovePowerPlantCard (index, drawDeckPowerPlants.Count);
+
+		//put 13 on top
+		index = FindIndexOfPowerPlantCard (13);
+		if (index != -1)
+			MovePowerPlantCard (index, 0);
+
+	}
+	
+	void InitializePowerPlants() {
+		drawDeckPowerPlants = new ArrayList ();
+		inMarketPowerPlants = new ArrayList ();
+
+		//create power plant cards
+		PowerPlant[] plants = FindObjectsOfType(typeof(PowerPlant)) as PowerPlant[];
+		for (int i = 0; i < plants.Length; i++) {
+			drawDeckPowerPlants.Add (plants [i]);
+		}
+
+		drawDeckPowerPlants.Sort ();
+	}
+
+	void DealCards() {
+		while (inMarketPowerPlants.Count < marketCount) {
+			inMarketPowerPlants.Add(drawDeckPowerPlants[0]);
+			drawDeckPowerPlants.RemoveAt(0);
+		}	
+
+		inMarketPowerPlants.Sort ();
+	}
+
+	void DoBureaucracy() {
+		foreach (Player p in players) {
+			int powerCount = 0;
+
+			foreach(PowerPlant pp in p.powerPlants)
+				powerCount += pp.RunPlant();
+			if(powerCount > 20)
+				powerCount = 20;
+			p.cash += cityCountPayoutTable[powerCount];
+			p.cash += Random.Range(0,20);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {	
+		bool advanceState = false;
+		bool advanceTurn = false;
+
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			advanceTurn = true;
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Reset();
+		}
+
+
+		switch (currentState) {
+		case  State.ComputeTurn:
+//			print (currentState);
+			players.Sort();
+			players.Reverse ();
+
+			currentState = State.BuyPlants;
+			playerTurn = 0;
+			break;
+		case  State.BuyPlants:
+			//show plants
+			//click on plant or pass
+			//adjust price
+			//click on player that buys
+			//redraw
+			if (advanceTurn) {
+				playerTurn++;
+				if(playerTurn == players.Count) {
+					advanceState = true;
+					playerTurn = players.Count-1;
+				}
+			}
+
+			if(advanceState) {
+				DealCards();
+				currentState = State.BuyMaterials;
+//				print (currentState);
+			}
+		break;
+		case  State.BuyMaterials:
+			//show each player's power plants
+			//show market
+			//allow buying into each plant by clicking
+			if (advanceTurn) {
+				playerTurn--;
+				if(playerTurn == -1) {
+					advanceState = true;
+					playerTurn = players.Count-1;
+				}
+			}
+
+			if(advanceState) {
+				currentState = State.BuildCities;
+//				print (currentState);
+			}
+		break;
+		case  State.BuildCities:
+			//hovering over city to shows cost (compute cost by searching over graph)
+
+			//click to buy
+			if (advanceTurn) {
+				playerTurn--;
+				if(playerTurn == -1) {
+					advanceState = true;
+					playerTurn = 0;
+				}
+			}
+			if(advanceState) {
+				currentState = State.Bureaucracy;
+//				print (currentState);
+			}
+		break;
+		case  State.Bureaucracy:
+//			print (currentState);
+			DoBureaucracy();
+			materialStore.Restock(players.Count, gameStep);
+			currentState = State.ComputeTurn;
+			gameRound++;
+			break;
+		}
+
+
+		int index = 0;
+		foreach (Player p in players) {
+			if(index == playerTurn) {
+				p.gameObject.GetComponent<Renderer>().material.color = Color.red;
+			} else {
+				p.gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+			}
+			index++;
+		}
+
+		foreach (PowerPlant pp in drawDeckPowerPlants) {
+			pp.gameObject.GetComponent<Renderer>().material.color = Color.gray;
+		}
+		index = 0;
+		foreach (PowerPlant pp in inMarketPowerPlants) {
+			if(index < biddableCount)
+				pp.gameObject.GetComponent<Renderer>().material.color = Color.green;
+			else
+				pp.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			index++;
+		}
+
+	}
+
+	void OnGUI() {
+
+		int yPos = 10;
+		GUI.Label (new Rect (10, yPos, 500, 20), "Round: " + gameRound + 
+            " Step: " + gameStep + 
+			" State: " + currentState + 
+			" PlayerTurn: " + ((Player)players [playerTurn]).gameObject.name);
+		yPos += 20;
+		GUI.Label(new Rect(10,yPos,500,20),players[0].ToString());
+		yPos += 20;
+		GUI.Label(new Rect(10,yPos,500,20),players[1].ToString());
+		yPos += 20;
+		GUI.Label(new Rect(10,yPos,500,20),players[2].ToString());
+		yPos += 20;
+		GUI.Label(new Rect(10,yPos,500,20),players[3].ToString());
+		yPos += 20;
+
+		GUI.Label(new Rect(10,yPos,500,20),"Material Store: " +
+		          " Coal: [" + materialStore.QueryInventory(PowerPlantMaterialStore.Type.Coal) + "," + materialStore.QueryCost(PowerPlantMaterialStore.Type.Coal)+"]" +
+		          " Oil: [" + materialStore.QueryInventory(PowerPlantMaterialStore.Type.Oil) + "," + materialStore.QueryCost(PowerPlantMaterialStore.Type.Oil)+"]" +
+		          " Garbage: [" + materialStore.QueryInventory(PowerPlantMaterialStore.Type.Garbage) + "," + materialStore.QueryCost(PowerPlantMaterialStore.Type.Garbage)+"]" +
+		          " Uranium: [" + materialStore.QueryInventory(PowerPlantMaterialStore.Type.Uranium) + "," + materialStore.QueryCost(PowerPlantMaterialStore.Type.Uranium)+"]");
+		yPos += 20;
+
+		int count = 0;
+		foreach (PowerPlant pp in inMarketPowerPlants) {
+			if(count < biddableCount)
+				GUI.Label (new Rect (10, yPos, 500, 20), "Biddable: " + pp.ToString ());
+			else
+				GUI.Label (new Rect (10, yPos, 500, 20), "On Deck: " + pp.ToString ());
+			count++;
+			yPos += 20;
+		}
+
+	}
+}
